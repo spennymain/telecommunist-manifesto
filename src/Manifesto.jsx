5b8ce870d682c33b134912ac8daab5643139d2fe
Spencer Main <spencer.main@bnc.ca>
2020-08-04 14:00:35 -0400

    establishes the legitimacy of the application

---
+++ src/Manifesto.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { hot } from 'react-hot-loader'
+class Manifesto extends React.Component {
+  render() {
+		return (
+			<div><p>hello there comrade</p></div>
+		)
+  }
+}
+export default hot(module)(Manifesto)
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Manifesto from './Manifesto'
+
+ReactDOM.render(<Manifesto />, document.getElementById('root'))
